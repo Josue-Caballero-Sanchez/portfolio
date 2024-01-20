7d107ef37f30434c212c3c365234fadce9ac1007
Josue-Caballero-Sanchez <sanchez684.jc@gmail.com>
2024-01-20 14:35:38 -0700

    fixed header animations

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const main = (() =>{
+
     const headerToggleButton = document.querySelector(".header-toggle-button");
     const navbarLinks = document.querySelector(".navbar-links");
 
     headerToggleButton.addEventListener("click", () =>{
-        navbarLinks.classList.toggle("active");
+        if (navbarLinks.classList.contains("active")) {
+            navbarLinks.classList.remove("active");
+            navbarLinks.classList.add("active-reverse");
+
+            navbarLinks.addEventListener("animationend", () => {
+                navbarLinks.style.display = "none";
+            }, { once: true });
+        } 
+        else {
+            navbarLinks.classList.remove("active-reverse");
+            navbarLinks.classList.add("active");
+
+            navbarLinks.style.display = "flex";
+        }
     });
 })();
